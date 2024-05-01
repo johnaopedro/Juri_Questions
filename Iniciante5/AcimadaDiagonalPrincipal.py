5b59bce461a9820c9cd02027217d904dd1ded61e
John <johnaopedro@gmail.com>
2024-05-01 14:55:17 -0300

    3xMatriz

---
+++ Iniciante5/AcimadaDiagonalPrincipal.py
@@ -0,0 +1,41 @@
+def criar_matriz(tamanho):
+    soma_media = 0
+    matriz = []
+    valores = []
+    cont=0
+    for _ in range(tamanho):
+        linha = []
+        for _ in range(tamanho):
+            linha.append(0)
+        matriz.append(linha)
+
+    ordem = input()
+    for i in range(tamanho * tamanho):
+        valor = input()
+        valores.append(valor)
+    for elementos in range(len(valores)):
+        valores[elementos] = float(valores[elementos])
+
+    for i in range(tamanho):
+        for j in range(tamanho - i):
+            matriz[i][j] = valores[i * tamanho + j + i]
+
+    for i in range(len(matriz)):
+        for j in range(1, tamanho - i):
+            soma_media += matriz[i][j]
+            cont+=1
+
+    if ordem == 'S':
+        soma_media = soma_media
+    elif ordem == 'M':
+        soma_media = soma_media / cont
+
+    return soma_media
+
+def main():
+    tamanho = 12
+    soma_media = criar_matriz(tamanho)
+    print("%.1f" % soma_media)
+
+if __name__ == "__main__":
+    main()
---
+++ Iniciante5/ColunanaMatriz.py
@@ -0,0 +1,37 @@
+def criar_matriz(tamanho):
+    matriz=[]
+    valores=[]
+    for _ in range(tamanho):
+        linha = []
+        for _ in range(tamanho):
+            linha.append(0)
+        matriz.append(linha)
+
+    referencia_linha = int(input())
+    ordem = input()
+    
+    for i in range(tamanho*tamanho):
+        valor = input()
+        valores.append(valor)
+    for elementos in range(len(valores)):
+        valores[elementos] = float(valores[elementos])
+
+    for i in range(tamanho): #SO MUDAR AQUI PARA COLUNA
+        for j in range(tamanho):
+            matriz[j][i] = valores[i * tamanho + j]
+
+    soma_media = sum(matriz[referencia_linha]) #Soma Tudo
+    if ordem == 'S':
+        soma_media=soma_media
+    elif ordem == 'M':
+        soma_media = soma_media/tamanho
+
+    return matriz, soma_media
+def main():
+    tamanho = 12
+    resultado = criar_matriz(tamanho)
+    if resultado:
+        matriz, soma_media = resultado #Matriz pega matriz e soma a soma
+        print("%.1f"%soma_media)
+if __name__ == "__main__":
+    main() 
